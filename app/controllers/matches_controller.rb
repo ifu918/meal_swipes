5e8540733383bfdbc967b5bc1b8577002090177c
Raghu Betina <raghu@firstdraft.com>
2019-12-08 23:55:58 +0000

    Only match user can modify match

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,4 +1,14 @@
 class MatchesController < ApplicationController
+  before_action :current_user_must_be_match_creator, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_match_creator
+    match = Match.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == match.creator
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @matches = Match.all
 
