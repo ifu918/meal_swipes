d465e3114f28fbf3a78b5a559e196d567cb5901d
Raghu Betina <raghu@firstdraft.com>
2019-12-08 23:55:58 +0000

    Whitelist columns in Match for ActiveAdmin

--- app/admin/matches.rb
+++ app/admin/matches.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Match do
+
+ permit_params :seller, :buyer, :dining_hall, :price, :meeting_time
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
